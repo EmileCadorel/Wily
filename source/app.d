f4ff49a4deb1e5e5ebe8b7d412bd2d2729a0ccf7
Emile Cadorel <ecadorel@gmail.com>
2017-03-01 12:36:38 +0100

    app.d

--- source/app.d
+++ source/app.d
@@ -4,32 +4,16 @@ import ast.Program;
 import utils.WilyException;
 
 void main(string[] args) {
-
-
-    import std.stdio;
-    import utils.Colors, syntax.Word;
-    import ast.all;
-    import parent = analyse.parent.Visitor;
-    
-    immutable string fail = Colors.RED.value ~ "Test Visitor fail" ~ Colors.RESET.value;
-    auto a = Word (Location (0, 0, 1, "", false), "a");
-    auto b = Word (Location (0, 0, 1, "", false), "b");
-    
-    auto varA = new Var (a), varB = new Var (b);
-    assert (!parent.Visitor.equals (varA, varB), fail);
-    
-    // if (args.length < 2) {
-    // 	writeln ("File name expected !");
-    // 	return -1;
-    // }
-
-    // try {
-    // 	Visitor visitor = new Visitor (args[1]);
-    // 	Program program = visitor.visit ();
-    // 	program.print ();
-    // } catch (WilyException e) {
-    // 	writeln (e);
-    // }
-
-    // return 0;
+    try {
+	if (args.length < 2) {
+	    throw new Exception ("(usage) %file");
+	}
+    	Visitor visitor = new Visitor (args[1]);
+    	Program program = visitor.visit ();
+    	program.print ();
+    } catch (WilyException e) {
+    	writeln (e);
+    } catch (Exception e) {
+	writeln (e.msg);
+    }
 }
