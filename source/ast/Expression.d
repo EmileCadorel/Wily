6e3b9a9393105e7c8bdbdcb8dee0ac87d1ac07f2
GuillaumeGas <guillaume@gas-ntic.fr>
2017-03-01 11:49:09 +0100

    Ajout du symbole dans l'ast expression

--- source/ast/Expression.d
+++ source/ast/Expression.d
@@ -1,11 +1,18 @@
 module ast.Expression;
 import ast.Instruction;
 import syntax.Word;
+import tables.Symbol;
 
 class Expression : Instruction {
     this (Word token) {
 	super (token);
     }    
 
+    public ref Symbol symbol () {
+	return _symbol;
+    }
+
     override void print (int nb = 0) {}
+
+    private Symbol _symbol;
 }
